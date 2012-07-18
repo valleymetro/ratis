13cb5ebcf9a0c51d39b44ee5ae2bfab839e30e93
Dave Tapley <dukedave@gmail.com>
2012-07-18 15:57:54 -0700

    Added spec for AtisRoute#timetable

--- spec/ratis/atis_route_spec.rb
+++ spec/ratis/atis_route_spec.rb
@@ -31,6 +31,33 @@ describe AtisRoute do
 
   end
 
+  describe '#timetable' do
+
+    let(:route) { AtisRoute.new '0', ['N', 'S'] }
+
+    before do
+      resp = atis_response_timetable({ route: '0', direction: 'N', service_type: 'W', operator: 'OP', effective: '01/15/12' })
+      stub_atis_request.to_return( atis_response 'Timetable', '1.1', '0', resp)
+
+      @timetable = route.timetable direction: 'N', service_type: 'W'
+    end
+
+    it 'only makes one request' do
+      an_atis_request.should have_been_made.times 1
+    end
+
+    it 'assigns settings correctly' do
+      @timetable.should_not be_nil
+
+      @timetable.route_short_name.should eql '0'
+      @timetable.direction.should eql 'N'
+      @timetable.service_type.should eql 'W'
+      @timetable.operator.should eql 'OP'
+      @timetable.effective.should eql '01/15/12'
+    end
+
+  end
+
   it 'should initialize' do
     route = AtisRoute.new '123', ['N', 'S']
 
