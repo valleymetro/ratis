e72be712e53e399bc0fc6d690815142c2b61367e
Dave Tapley <dukedave@gmail.com>
2012-07-17 19:00:13 -0700

    Added rspec tests and helpers for AtisTimetable.

---
+++ spec/ratis/atis_timetable_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe AtisTimetable do
+
+  describe '#where' do
+
+    before do
+      resp = atis_response_timetable({ route: '0', direction: 'N', service_type: 'W', operator: 'OP', effective: '01/15/12' })
+      stub_atis_request.to_return( atis_response 'Timetable', '1.1', '0', resp)
+
+      @timetable = AtisTimetable.where route_short_name: '0', direction: 'N', service_type: 'W'
+    end
+
+    it 'only makes one request' do
+      an_atis_request.should have_been_made.times 1
+    end
+
+    it 'assigns settings correctly' do
+      @timetable.should_not be_nil
+
+      @timetable.route_short_name.should eql '0'
+      @timetable.direction.should eql 'N'
+      @timetable.service_type.should eql 'W'
+      @timetable.operator.should eql 'OP'
+      @timetable.effective.should eql '01/15/12'
+    end
+
+  end
+
+end
+
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,18 +18,55 @@ end
 
 def atis_response action, version, action_response_code, action_response_body
   { body: <<-BODY }
-  <?xml version="1.0" encoding="UTF-8"?>
-  <SOAP-ENV:Envelope xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/1999/XMLSchema" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-    <SOAP-ENV:Body>
-      <namesp1:#{action}Response xmlns:namesp1="PX_WEB">
-        <Responsecode>#{action_response_code}</Responsecode>
-        <Version>#{version}</Version>
-        #{action_response_body}
-        <Copyright>XML schema Copyright (c) 2011 Trapeze Software, Inc., all rights reserved.</Copyright>
-        <Soapversion>2.4.4 - 08/31/11</Soapversion>
-      </namesp1:#{action}Response>
-    </SOAP-ENV:Body>
-  </SOAP-ENV:Envelope>
+<?xml version="1.0" encoding="UTF-8"?>
+<SOAP-ENV:Envelope xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/1999/XMLSchema" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+  <SOAP-ENV:Body>
+    <namesp1:#{action}Response xmlns:namesp1="PX_WEB">
+      <Responsecode>#{action_response_code}</Responsecode>
+      <Version>#{version}</Version>
+      #{action_response_body}
+      <Copyright>XML schema Copyright (c) 2011 Trapeze Software, Inc., all rights reserved.</Copyright>
+      <Soapversion>2.4.4 - 08/31/11</Soapversion>
+    </namesp1:#{action}Response>
+  </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
   BODY
 end
 
+def atis_response_timetable params = {}
+  <<-BODY
+<Headway>
+  <Route>#{params[:route] || '0' }</Route>
+  <Direction>#{ params[:direct] || 'N' }</Direction>
+  <Servicetype>#{ params[:service_type] || 'W' }</Servicetype>
+  <Signage>0 North to Road St.</Signage>
+  <Operator>#{ params[:operator] || 'OP' }</Operator>
+  <Effective>#{ params[:effective] || '01/15/2012' }</Effective>
+  <Timepoints>
+    <Stop>
+      <Atisstopid>1337</Atisstopid>
+      <Stopid>80085</Stopid>
+      <Description>ALPHA ST &amp; BETA RD</Description> 
+      <Area>Townsville</Area>
+    </Stop>
+  </Timepoints>
+<Times>
+  <Trip>
+    <Tripid>001-001</Tripid>
+    <Block>999</Block>
+    <Comment>My trip comment</Comment>
+    <Time>12:34 AM</Time>
+    <Calendarstartdate>#{ Time.now.strftime '%m/%d/%y' }</Calendarstartdate>
+    <Calendar>#{ 120.times.collect{['y','n'].sample} }</Calendar>
+    <Commentcode>comment_code_trip</Commentcode>
+  </Trip>
+</Times>
+</Headway>
+<Comments>
+<Comment>
+ <Code>comment_code_request</Code>
+ <Text>My request comment</Text>
+</Comment>
+</Comments>
+  BODY
+end
