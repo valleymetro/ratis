f2d364e7d8aa691bfbe61ed3971484d3016139c1
Dave Tapley <dukedave@gmail.com>
2012-07-19 16:07:16 -0700

    Added AtisStop and specs.

---
+++ lib/ratis/atis_stop.rb
@@ -0,0 +1,43 @@
+require 'ratis/atis_model'
+
+class AtisStop
+  extend AtisModel
+
+  attr_accessor :walkdist, :description, :stopid, :atisstopid, :latitude, :longitude, :walkdir, :side, :heading, :stopposition, :routedir
+
+  def self.closest(criteria)
+    latitude = criteria.delete :latitude
+    longitude = criteria.delete :longitude
+    location_text = criteria.delete :location_text
+    num_stops = criteria.delete :num_stops
+
+    raise ArgumentError.new('You must provide a longitude') unless longitude
+    raise ArgumentError.new('You must provide a latitude') unless latitude
+
+    response = atis_request 'Closeststop',
+      {'Locationlat' => latitude, 'Locationlong' => longitude, 'Locationtext' => location_text, 'Numstops' => num_stops}
+
+    return [] unless response.success?
+
+    stops = response.to_hash[:closeststop_response][:stops][:stop].map do |s|
+      next if s[:description].blank?
+
+      stop = AtisStop.new
+      stop.walkdist = s[:walkdist]
+      stop.description = s[:description]
+      stop.stopid = s[:stopid]
+      stop.atisstopid = s[:atisstopid]
+      stop.latitude = s[:lat]
+      stop.longitude = s[:long]
+      stop.walkdir = s[:walkdir]
+      stop.side = s[:side]
+      stop.heading = s[:heading]
+      stop.stopposition = s[:stopposition]
+      stop.routedir = s[:routedirs][:routedir]
+      stop
+    end
+    stops.compact
+  end
+
+end
+
---
+++ spec/ratis/atis_stop_spec.rb
@@ -0,0 +1,94 @@
+require 'spec_helper'
+
+describe AtisStop do
+
+  describe '#closest' do
+
+    before do
+      stub_atis_request.to_return atis_response 'Closeststop', '1.11', '0', <<-BODY
+      <Input>
+        <Locationlat>33.4556</Locationlat>
+        <Locationlong>-112.071807</Locationlong>
+        <Locationtext>Text of the location as "located" using the locate message</Locationtext>
+        <Numstops>15</Numstops>
+        <Distance>0</Distance>
+        <Accessible>Y</Accessible>
+        <Xmode> BCXTFRSLK123 </Xmode>
+      </Input>
+      <Stops>
+        <Stop>
+          <Walkdist>0.1</Walkdist>
+          <Description>text description stop one</Description>
+          <Stopid>12345</Stopid>
+          <Atisstopid>66666</Atisstopid>
+          <Lat>33.4576</Lat>
+          <Long>-112.071507</Long>
+          <Walkdir>NE</Walkdir>
+          <Stopposition>N</Stopposition>
+          <Heading>NB</Heading>
+          <Side>Far</Side>
+          <Routedirs>
+            <Routedir>0-N</Routedir>
+          </Routedirs>
+        </Stop>
+        <Stop>
+          <Walkdist>0.5</Walkdist>
+          <Description>text description of stop two</Description>
+          <Stopid>12345</Stopid>
+          <Atisstopid>66666</Atisstopid>
+          <Lat>33.4356</Lat>
+          <Long>-112.071607</Long>
+          <Walkdir>S</Walkdir>
+          <Stopposition>S</Stopposition>
+          <Heading>SB</Heading>
+          <Side>Near</Side>
+          <Routedirs>
+            <Routedir>6-W</Routedir>
+          </Routedirs>
+        </Stop>
+      </Stops>
+      BODY
+
+      @stops = AtisStop.closest latitude: '33.4556', longitude: '-112.071807', location_text: 'some location text', num_stops: 15
+      @first_stop = @stops.first
+    end
+
+    it 'only makes one request' do
+      an_atis_request.should have_been_made.times 1
+    end
+
+    it 'requests the correct SOAP action' do
+      an_atis_request_for(
+        'Closeststop',
+        'Locationlat' => '33.4556', 'Locationlong' => '-112.071807',
+        'Locationtext' => 'some location text',
+        'Numstops' => '15'
+      ).should have_been_made
+    end
+
+    it 'returns multiple locations' do
+      @stops.should have(2).items
+    end
+
+    it 'ignores a <Stop> without a blank <Description>' do
+      pending 'not sure if this is the correct behaviour'
+    end
+
+    it 'parses out fields correctly' do
+      @first_stop.walkdist.should eql '0.1'
+      @first_stop.description.should eql 'text description stop one'
+      @first_stop.stopid.should eql '12345'
+      @first_stop.atisstopid.should eql '66666'
+      @first_stop.latitude.should eql '33.4576'
+      @first_stop.longitude.should eql '-112.071507'
+      @first_stop.walkdir.should eql 'NE'
+      @first_stop.side.should eql 'Far'
+      @first_stop.heading.should eql 'NB'
+      @first_stop.stopposition.should eql 'N'
+      @first_stop.routedir.should eql '0-N'
+    end
+
+  end
+
+end
+
