6ee8d9f84e3d4f4829705a3bc1ef1a8734f251a6
swedler <steve.swedler@gmail.com>
2012-08-16 15:14:36 -0700

    Scoping stop classes appropriately. Not sure if it would be better to have one stop class extended with all possible attributes which would be nil if unavailable. Reserve the right to consolidate later.

--- config/valley_metro.rb
+++ config/valley_metro.rb
@@ -1,9 +1,10 @@
-require 'ratis/config'
+#This stuff goes in initializers/ratis.rb
 
-Ratis.configure do |config|
-  config.endpoint = 'http://soap.valleymetro.org/cgi-bin-soap-web-new/soap.cgi'
-  config.namespace = 'PX_WEB'
-  config.proxy = 'http://localhost:8080'
-  config.timeout = 5
-end
+# require 'ratis/config'
+
+# Ratis.configure do |config|
+#   config.endpoint = 'http://soap.valleymetro.org/cgi-bin-soap-web-new/soap.cgi'
+#   config.namespace = 'PX_WEB'
+#   config.timeout = 5
+# end
 
