598af088647620e4eb87ad4c6bfb65e61ff227b6
swedler <steve.swedler@gmail.com>
2013-01-14 10:05:43 -0700

    downgraded to savon <2 so we don't have to deal with that shit

--- lib/ratis/request.rb
+++ lib/ratis/request.rb
@@ -7,8 +7,6 @@ module Ratis
     def initialize(config = nil)
       config = Ratis.config if config.nil?
       raise Errors::ConfigError('It appears that Ratis.configure has not been called') unless config.valid?
-      # UPDATED SAVON NEEDS SOMETHING LIKE ...
-      # Savon.client(endpoint: 'http://soap.valleymetro.org/cgi-bin-soap-web-252/soap.cgi', namespace: 'PX_WEB')
       self.class.client do
         wsdl.endpoint     = Ratis.config.endpoint
         wsdl.namespace    = Ratis.config.namespace
@@ -31,7 +29,7 @@ module Ratis
         response
       rescue Errno::ECONNREFUSED => e
         raise Errno::ECONNREFUSED.new 'Refused request to ATIS SOAP server'
-      rescue Savon::SOAPFault => e
+      rescue Savon::SOAP::Fault => e
         raise Errors::SoapError.new e
       end
     end
